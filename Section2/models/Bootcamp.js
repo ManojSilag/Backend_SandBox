a8fedfcc73427093fa8bc01098dc9059af7a1ce5
ManojSilag <manojsilag@gmail.com>
2019-11-09 16:23:17 +0530

    update

--- Section2/models/Bootcamp.js
+++ Section2/models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
           name: {
@@ -97,5 +98,14 @@ const BootcampSchema = new mongoose.Schema({
             default: Date.now
           }
 });
+
+//Create Bootcamp slug from name
+BootcampSchema.pre('save', function(next){
+  // console.log('slugify ran', this.name);
+
+  this.slug = slugify(this.name, { lower: true })
+  next();
+  
+})
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
