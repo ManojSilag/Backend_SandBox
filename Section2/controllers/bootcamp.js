f70176b6511c375fea92318892705dd2b3688b11
Manoj Silag <silagmanoj@yahoo.in>
2019-11-12 21:46:54 +0530

    Update bootcamp.js

--- Section2/controllers/bootcamp.js
+++ Section2/controllers/bootcamp.js
@@ -2,7 +2,6 @@ const Bootcamp = require('../models/Bootcamp');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('./../middleware/async');
 
-
 // @desc     Get all bootcamps
 // @route    GET /api/v1/bootcamps
 // @acess    Public
